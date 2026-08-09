065a0bb7642807d110d1e787806ae835b9a88717
shivmSingh1 <shivams7380@gmail.com>
2026-08-09 07:19:08 +0000

    hide users from managers

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -16,33 +16,42 @@ import { logout } from '../http/api';
 
 const { Sider, Header, Content, Footer } = Layout;
 
-const items = [
-	{
-		key: '/',
-		icon: <Icon component={HomeIcon} />,
-		label: <NavLink to="/">Home</NavLink>,
-	},
-	{
-		key: '/users',
-		icon: <Icon component={UserIcon} />,
-		label: <NavLink to="/users">Users</NavLink>,
-	},
-	{
-		key: '/restaurants',
-		icon: <Icon component={foodIcon} />,
-		label: <NavLink to="/restaurants">Restaurants</NavLink>,
-	},
-	{
-		key: '/products',
-		icon: <Icon component={BasketIcon} />,
-		label: <NavLink to="/products">Products</NavLink>,
-	},
-	{
-		key: '/promos',
-		icon: <Icon component={GiftIcon} />,
-		label: <NavLink to="/promos">Promos</NavLink>,
-	},
-];
+const getMenuItems = (role: string) => {
+	const baseItems = [
+		{
+			key: '/',
+			icon: <Icon component={HomeIcon} />,
+			label: <NavLink to="/">Home</NavLink>,
+		},
+		{
+			key: '/restaurants',
+			icon: <Icon component={foodIcon} />,
+			label: <NavLink to="/restaurants">Restaurants</NavLink>,
+		},
+		{
+			key: '/products',
+			icon: <Icon component={BasketIcon} />,
+			label: <NavLink to="/products">Products</NavLink>,
+		},
+		{
+			key: '/promos',
+			icon: <Icon component={GiftIcon} />,
+			label: <NavLink to="/promos">Promos</NavLink>,
+		},
+	];
+
+	if (role === 'admin') {
+		const menus = [...baseItems];
+		menus.splice(1, 0, {
+			key: '/users',
+			icon: <Icon component={UserIcon} />,
+			label: <NavLink to="/users">Users</NavLink>,
+		});
+		return menus;
+	}
+
+	return baseItems;
+};
 
 const Dashboard = () => {
 
@@ -67,6 +76,9 @@ const Dashboard = () => {
 	if (user === null) {
 		return <Navigate to="/auth/login" replace={true} />;
 	}
+
+	const items = getMenuItems(user.role);
+
 	return (
 		<div>
 			<Layout style={{ minHeight: '100vh', background: colorBgContainer }}>
